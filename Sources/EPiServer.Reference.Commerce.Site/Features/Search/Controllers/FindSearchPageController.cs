8750884dbfe98d21323a6ddb2326a884fdd8feb6
Cauldrath <ben@brandingbrand.com>
2019-08-12 16:07:33 -0400

    feat: Add Episerver Find
    TEST
    -------------
    
    - go to /findsearchpage/?q=shoes&page=1
    - should show you the first page of search results from Find for "shoes"

---
+++ Sources/EPiServer.Reference.Commerce.Site/Features/Search/Controllers/FindSearchPageController.cs
@@ -0,0 +1,32 @@
+﻿using EPiServer.Find;
+using EPiServer.Find.Framework;
+using EPiServer.Reference.Commerce.Site.Features.Search.Pages;
+using EPiServer.Reference.Commerce.Site.Features.Search.ViewModels;
+using EPiServer.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Search.Controllers
+{
+    public class FindSearchPageController : PageController<SearchPage>
+    {
+        public int pageSize = 10;
+
+        public ActionResult Index(SearchPage currentPage, string q, int page = 1)
+        {
+            var model = new FindSearchPageViewModel(currentPage, q, page);
+            if (String.IsNullOrEmpty(q))
+            {
+                return View(model);
+            }
+            var unifiedSearch = SearchClient.Instance.UnifiedSearchFor(q);
+            model.Results = unifiedSearch.Skip((page - 1) * pageSize).Take(pageSize).Filter(x => 
+                x.SearchTypeName.Match("Product")
+            ).GetResult();
+            return View(model);
+        }
+    }
+}
---
+++ Sources/EPiServer.Reference.Commerce.Site/Features/Search/ViewModels/FindSearchPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using EPiServer.Find.UnifiedSearch;
+using EPiServer.Reference.Commerce.Site.Features.Search.Pages;
+using EPiServer.Reference.Commerce.Site.Features.Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Search.ViewModels
+{
+    public class FindSearchPageViewModel : PageViewModel<SearchPage>
+    {
+        public FindSearchPageViewModel(SearchPage currentPage, string searchQuery, int page)
+        {
+            CurrentPage = currentPage;
+            SearchQuery = searchQuery;
+            Page = page;
+        }
+        public string SearchQuery { get; private set; }
+        public int Page { get; set;  }
+        public UnifiedSearchResults Results { get; set; }
+    }
+}
